ffaa4d9cd7974c32b7fd4c946210773ffe729ccf
pnikolidakis <pnikolidakis@procureship.com>
2024-03-24 13:17:02 +0200

    import css, change title and use App.tsx instead of main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
 import './App.css'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import LoginPage from "./components/login_pages/LoginPage.tsx";
+import ErrorPage from "./components/error_pages/ErrorPage.tsx";
+import MoviesPage from "./components/secured/MoviesPage.tsx";
+import {SignUpPage} from "./components/login_pages/SignUpPage.tsx";
+import {NoAccessPage} from "./components/error_pages/NoAccessPage.tsx";
+import {AuthProvider} from "./components/AuthContext.tsx";
+import Home from "./components/Home.tsx";
 
-function App() {
-  const [count, setCount] = useState(0)
+const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
+);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+const router = createBrowserRouter([
+        {
+            path: "/",
+            element: <Home/>,
+            errorElement: <ErrorPage/>
+        },
+        {
+            path: "/secured",
+            element: <MoviesPage/>
+        },
+        {
+            path: '/login',
+            element: <LoginPage/>
+        },
+        {
+            path: "/sign-up",
+            element: <SignUpPage/>
+        },
+        {
+            path: "/error-no-access",
+            element: <NoAccessPage/>
+        }
+    ])
+;
 
-export default App
+root.render(
+    <React.StrictMode>
+        <AuthProvider>
+            <RouterProvider router={router}/>
+        </AuthProvider>
+    </React.StrictMode>
+);
